a889f51a4645e460c013f64ffe8bec119c66d88c
Glen Maddern <glenmaddern@gmail.com>
2017-09-16 16:44:47 +0530

    ooooooooh baby 3 tests passing

--- src/dank/parser.js
+++ src/dank/parser.js
@@ -13,34 +13,38 @@ let replacements
 stylis.use((context, content, selectors, parent, line, column, length) => {
   console.log([context, content, selectors, parent, line, column, length])
   if (context !== 1) return
-  const dollar_index = content.indexOf('$')
   let new_content
 
-  if (dollar_index === -1) {
-    return
-  } else if (dollar_index === 0) {
+  if (content.startsWith('$')) {
     new_content = `\${ ${content.slice(1) } }`
   } else {
-    const before_dollar = content.slice(0, dollar_index)
-    const after_dollar = content.slice(dollar_index + 1)
-    console.log({before_dollar, after_dollar})
-    const tokens = after_dollar.split(/\s+/)
+    const match = content.match(/\s\$[\w.]+(\s|$)/)
+    if (match) {
+      console.log(match)
+      const before_dollar = content.slice(0, match.index + 1)
+      const after_dollar = content.slice(match.index + 2)
+      console.log({ before_dollar, after_dollar })
+      const tokens = after_dollar.split(/\s+/)
 
-    const beginning = after_dollar.startsWith('props') ? `\${props => `
-      : after_dollar.startsWith('theme') ? `\${props => props.`
-        : `\${`
+      const beginning = after_dollar.startsWith('props') ? `\${props => `
+        : after_dollar.startsWith('theme') ? `\${props => props.`
+          : `\${`
 
-    let ended = false
-    let token
-    const expr_tokens = []
-    while (!ended && (token = tokens.shift())) {
-      if (expr_tokens.length === 0) {
-        expr_tokens.push(token)
-      } else {
+      let ended = false
+      let token
+      const expr_tokens = []
+      while (!ended && (token = tokens.shift())) {
+        if (expr_tokens.length === 0) {
+          expr_tokens.push(token)
+        } else {
 
+        }
+      }
+      if (expr_tokens.length > 0) {
+        new_content = `${before_dollar}${beginning}${expr_tokens.join(' ')}}${tokens.join(' ')}`
       }
     }
-    new_content = `${before_dollar}${beginning}${expr_tokens.join(' ')}}${tokens.join(' ')}`
+
   }
 
   if (new_content) {
