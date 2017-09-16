c5f2e812945da30e239c92333d47e29b8863887e
Glen Maddern <glenmaddern@gmail.com>
2017-09-16 13:28:32 +0530

    oh yeah i got super stuck

--- src/dank/parser.js
+++ src/dank/parser.js
@@ -1,11 +1,32 @@
 class Or {
-  constructor() {
+  constructor(lhs) {
+    this.lhs = lhs
     this.ended = false
-
+    this.contents = ''
   }
 
   next(token) {
+    console.log(this.contents)
+    console.log(token)
+    if (this.contents === null && !token.startsWith(`'`) && !token.startsWith(`"`)) {
+      this.contents = `'${token}'`
+    } else {
+      this.contents = `${this.contents}${token}`
+    }
+    if (
+      (this.contents.startsWith(`'`) && !this.contents.endsWith(`'`))
+    ||
+      (this.contents.startsWith(`"`) && !this.contents.endsWith(`"`))
+    ) {
+      // keep consuming
+      return this
+    } else {
+      return new Start(`${this.lhs.token} || ${this.contents}`)
+    }
+  }
 
+  toString() {
+    return this.lhs.toString(`|| ${this.contents}`)
   }
 }
 
@@ -16,7 +37,8 @@ class Start {
   }
 
   next(token) {
-    if (token === ';') this.ended = true
+    if (token.match(/^\s+$/)) {}
+    else if (token === ';') this.ended = true
     else if (token === '||') {
       return new Or(this)
     }
@@ -25,7 +47,7 @@ class Start {
 
   toString() {
     const no_dollar = this.token.substr(1, this.token.length - 1)
-    if (no_dollar.startsWith('$props')) {
+    if (no_dollar.startsWith('props')) {
       return `\${props => ${no_dollar}}`
     } else if (no_dollar.startsWith('theme')) {
       return `\${props => props.${no_dollar}}`
@@ -38,8 +60,8 @@ class Start {
 export default str => {
   let current_expression = null
 
-  return str.replace(/[\w$.:|{}?]+(?=[\s;])|;|\s\?\s/gm, token => {
-    //console.log({ token, current_expression })
+  return str.replace(/[\w$.:|{}?'"]+(?=[\s;])|;|\s+/gm, token => {
+    console.log({ token, current_expression })
     if (current_expression === null) {
       if (token.startsWith('$')) {
         current_expression = new Start(token)
--- test/dank.test.js
+++ test/dank.test.js
@@ -42,4 +42,26 @@ describe('dank parser', () => {
       foo: $bar.baz($omg, $ffs);
     `)
   })
+
+  it('should allow props and theme interpolations', () => {
+    expect(parser(`
+      foo: $props.x;
+      bar: $theme.y;
+    `)).toEqual(`
+      foo: \${props => props.x};
+      bar: \${props => props.theme.y};
+    `)
+  })
+
+  it('should allow fallbacks', () => {
+    expect(parser(`
+      foo: $x || 0.5;
+      bar: $props.y || lol;
+      baz: $theme.z || 'x y z';
+    `)).toEqual(`
+      foo: \${x || '0.5'};
+      bar: \${props => props.x || 'lol'};
+      baz: \${props => props.theme.z || 'x y z'};
+    `)
+  })
 })
