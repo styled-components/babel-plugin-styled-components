196d9ab2b77c50d74f8651997de32b39698ab694
Rungsikorn Rungsikavarnich <rungsikorn@icloud.com>
2017-10-12 20:27:36 +0700

    add comment for specific detector

--- src/utils/detectors.js
+++ src/utils/detectors.js
@@ -14,13 +14,27 @@ const importLocalName = (name, state) => {
 }
 
 export const isStyled = (tag, state) => {
-  if (t.isCallExpression(tag) && t.isMemberExpression(tag.callee) && tag.callee.property.name !== 'default') {
+  if (t.isCallExpression(tag) && t.isMemberExpression(tag.callee) && tag.callee.property.name !== 'default' /** ignore default for #93 below */) {
     // styled.something()
     return isStyled(tag.callee.object, state)
   } else {
     return (
       (t.isMemberExpression(tag) && tag.object.name === importLocalName('default', state)) ||
       (t.isCallExpression(tag) && tag.callee.name === importLocalName('default', state)) ||
+
+      /**
+       * Support Typescript import #93
+       * Typescript transpile output come with member expression `default`
+       * ex.
+       * ------
+       * // Component.ts (before)
+       *  import styled from 'styled-component';
+       *  const Input = styled.input`width:100%;`
+       * ------
+       * // Component.js (transpiled)
+       *  const styled_components_1 = require('styled-component');
+       *  const Input = styled.default.input`width:100%;`
+       */
       (state.styledRequired && t.isMemberExpression(tag) && t.isMemberExpression(tag.object) && tag.object.property.name === 'default' && tag.object.object.name === state.styledRequired) ||
       (state.styledRequired && t.isCallExpression(tag) && t.isMemberExpression(tag.callee) && tag.callee.property.name === 'default' && tag.callee.object.name === state.styledRequired)
     )
