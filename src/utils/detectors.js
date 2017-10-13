cf5c21d656d15a5223440b3ccec67dd60510b883
Rungsikorn Rungsikavarnich <rungsikorn@icloud.com>
2017-10-13 10:26:36 +0700

    update comment for support require()

--- src/utils/detectors.js
+++ src/utils/detectors.js
@@ -23,17 +23,11 @@ export const isStyled = (tag, state) => {
       (t.isCallExpression(tag) && tag.callee.name === importLocalName('default', state)) ||
 
       /**
-       * Support Typescript import #93
-       * Typescript transpile output come with member expression `default`
-       * ex.
-       * ------
-       * // Component.ts (before)
-       *  import styled from 'styled-component';
-       *  const Input = styled.input`width:100%;`
-       * ------
-       * // Component.js (transpiled)
-       *  const styled_components_1 = require('styled-component');
-       *  const Input = styled.default.input`width:100%;`
+       * #93 Support require()
+       * styled-components might be imported using a require()
+       * call and assigned to a variable of any name.
+       * - styled.default.div``
+       * - styled.default.something()
        */
       (state.styledRequired && t.isMemberExpression(tag) && t.isMemberExpression(tag.object) && tag.object.property.name === 'default' && tag.object.object.name === state.styledRequired) ||
       (state.styledRequired && t.isCallExpression(tag) && t.isMemberExpression(tag.callee) && tag.callee.property.name === 'default' && tag.callee.object.name === state.styledRequired)
