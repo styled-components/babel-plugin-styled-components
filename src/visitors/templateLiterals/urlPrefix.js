c08c1faea10311473691ce4b15f5aa8541cdca71
Tim Kendall <timkendall@users.noreply.github.com>
2017-12-12 14:05:20 -0800

    Refactor urlPrefix visitor to be more readable

--- src/visitors/templateLiterals/urlPrefix.js
+++ src/visitors/templateLiterals/urlPrefix.js
@@ -4,6 +4,23 @@ import { useURLPrefix } from '../../utils/options'
 
 const RELATIVE_URL_REGEX = /(\/)(\w|\d|\s|\/)*(.svg|.png|.jpg|.jpeg|.gif)/g
 
+export const prefixRelativeURLs = (prefix) => (element) => {
+  const { type, value } = element
+  const relativeURLs = value.match(RELATIVE_URL_REGEX) || []
+
+  let transformedValue = value
+  for (let i = 0; i < relativeURLs.length; ++i) {
+    const relativeURL = relativeURLs[i]
+    const absoluteURL = prefix + relativeURL
+
+    // Note: Swap in the prefixed URL
+    transformedValue = transformedValue
+      .replace(relativeURL, absoluteURL)
+  }
+ 
+  return { type, value: transformedValue }
+}
+
 export default (path, state) => {
   if (
     isStyled(path.node.tag, state)
@@ -12,23 +29,8 @@ export default (path, state) => {
    
     const { tag: callee, quasi: { quasis, expressions }} = path.node
     const values = t.arrayExpression(quasis.map(quasi => t.stringLiteral(quasi.value.cooked)))
-    
-    const transformedElements = values.elements.map(elem => {
-      const { type, value } = elem
-      const relativeURLs = value.match(RELATIVE_URL_REGEX) || []
-
-      let transformedValue = value
-      for (let i = 0; i < relativeURLs.length; ++i) {
-        const relativeURL = relativeURLs[i]
-        const absoluteURL = urlPrefix + relativeURL
-
-        // Note: Swap in the prefixed URL
-        transformedValue = transformedValue
-          .replace(relativeURL, absoluteURL)
-      }
-     
-      return { type, value: transformedValue }
-    })
+    const prefixURLs = prefixRelativeURLs(urlPrefix)
+    const transformedElements = values.elements.map(prefixURLs)
 
     values.elements = transformedElements
 
