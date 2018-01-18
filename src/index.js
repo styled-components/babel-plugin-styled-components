f149841391f8ba0ba47a29dd65bf97f6dfcd2ba5
Osama Jandali <osama.jandali@robovics.com>
2018-01-18 22:33:52 +0200

    Pure comments specific for styled typed only
    - Move the function to separate file.
    - Check for isStyled and isHelper to only annotate them and not to mess
    up with normal calls
    - Add new two fixtures to test the new function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import annotateAsPure from "@babel/helper-annotate-as-pure"
-
+import uglifyPure from './visitors/uglifyPure'
 import minify from './visitors/minify'
 import displayNameAndId from './visitors/displayNameAndId'
 import templateLiterals from './visitors/templateLiterals'
 import assignStyledRequired from './visitors/assignStyledRequired'
 import { noParserImportDeclaration, noParserRequireCallExpression } from './visitors/noParserImport'
-import { useUglifyPure } from './utils/options'
+
 
 export default function({ types: t }) {
   return {
@@ -14,11 +13,7 @@ export default function({ types: t }) {
         noParserImportDeclaration(path, state)
       },
       CallExpression(path, state) {
-        if(useUglifyPure(state)){
-          if(path.parent.type == 'VariableDeclarator' || path.parent.type == 'TaggedTemplateExpression'){
-            annotateAsPure(path.node)
-          }
-        }
+        uglifyPure(path, state)
         noParserRequireCallExpression(path, state)
       },
       TaggedTemplateExpression(path, state) {
