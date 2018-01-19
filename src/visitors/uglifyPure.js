43c03ebdbe11127feb2c9f28e1eef031c4c5bc68
Osama Jandali <osama.jandali@robovics.com>
2018-01-19 23:47:28 +0200

    annotate injectGlobal
    - annotate injectGlobal
    - tests now include all cases (css, keyframes, injectGlobal)

--- src/visitors/uglifyPure.js
+++ src/visitors/uglifyPure.js
@@ -1,16 +1,18 @@
 import annotateAsPure from "@babel/helper-annotate-as-pure"
 
 import { useUglifyPure } from '../utils/options'
-import { isStyled, isHelper } from '../utils/detectors'
+import { isStyled, isHelper, isInjectGlobalHelper } from '../utils/detectors'
 
 export default (path, state) => {
     if(useUglifyPure(state)){
         if(isStyled(path.node,state) || 
           isStyled(path.node.callee,state) || 
-          isHelper(path.node.callee,state) 
+          isHelper(path.node.callee,state) ||
+          isInjectGlobalHelper(path.node.callee,state) 
         ){
             if(path.parent.type == 'VariableDeclarator' || 
-              path.parent.type == 'TaggedTemplateExpression'
+              path.parent.type == 'TaggedTemplateExpression' ||
+              path.parent.type =='ExpressionStatement'
             ){
                 annotateAsPure(path)
             }
