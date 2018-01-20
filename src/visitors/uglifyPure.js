391c366f7acb902ebf00e364f92f1c9ebbb26822
Osama Jandali <osama.jandali@robovics.com>
2018-01-20 15:41:30 +0200

    revert InjectGlobal annotation
    - Should not annotate InjectGlobal.
    - Remove InjectGlobal test fixture.

--- src/visitors/uglifyPure.js
+++ src/visitors/uglifyPure.js
@@ -1,18 +1,16 @@
 import annotateAsPure from "@babel/helper-annotate-as-pure"
 
 import { useUglifyPure } from '../utils/options'
-import { isStyled, isHelper, isInjectGlobalHelper } from '../utils/detectors'
+import { isStyled, isHelper } from '../utils/detectors'
 
 export default (path, state) => {
     if(useUglifyPure(state)){
         if(isStyled(path.node,state) || 
           isStyled(path.node.callee,state) || 
-          isHelper(path.node.callee,state) ||
-          isInjectGlobalHelper(path.node.callee,state) 
+          isHelper(path.node.callee,state) 
         ){
             if(path.parent.type == 'VariableDeclarator' || 
-              path.parent.type == 'TaggedTemplateExpression' ||
-              path.parent.type =='ExpressionStatement'
+              path.parent.type == 'TaggedTemplateExpression'
             ){
                 annotateAsPure(path)
             }
