63e36df7241c21cf9cec9935d2d39bab6dc07197
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-01-22 17:42:33 -0200

    Fixed importLocalName for multiple module names

--- src/utils/detectors.js
+++ src/utils/detectors.js
@@ -1,10 +1,12 @@
 import * as t from 'babel-types'
 import { getModuleName } from "./options"
 
-const importLocalName = (name, state) => {
-  let localName = name === 'default' ? 'styled' : name
+const importAllLocalNames = (name, state) => {
+  let localNames = {
+    default: name === 'default' ? 'styled' : name
+  }
   let moduleNames = getModuleName(state)
-  if(typeof moduleNames === "string") moduleNames  = [ moduleNames ]
+  if (typeof moduleNames === "string") moduleNames  = [ moduleNames ]
 
   state.file.path.traverse({
     ImportDeclaration: {
@@ -13,16 +15,11 @@ const importLocalName = (name, state) => {
 
         if (moduleNames.includes(node.source.value)) {
           for (const specifier of path.get('specifiers')) {
-            if (specifier.isImportDefaultSpecifier()) {
-              localName = specifier.node.local.name
-            }
-
-            if (specifier.isImportSpecifier() && specifier.node.imported.name === name) {
-              localName = specifier.node.local.name
-            }
-
-            if (specifier.isImportNamespaceSpecifier()) {
-              localName = specifier.node.local.name
+            if (
+              (specifier.isImportDefaultSpecifier()) ||
+              (specifier.isImportSpecifier() && specifier.node.imported.name === name) ||
+              (specifier.isImportNamespaceSpecifier())) {
+              localNames[node.source.value] = specifier.node.local.name
             }
           }
         }
@@ -30,17 +27,25 @@ const importLocalName = (name, state) => {
     }
   })
 
-  return localName
+  return localNames
 }
+
+const importLocalName = (name, state, moduleName) => {
+  const localNames = importAllLocalNames(name, state)
+  return localNames[moduleName] || localNames.default
+}
+  
 
 export const isStyled = (tag, state) => {
   if (t.isCallExpression(tag) && t.isMemberExpression(tag.callee) && tag.callee.property.name !== 'default' /** ignore default for #93 below */) {
     // styled.something()
     return isStyled(tag.callee.object, state)
   } else {
+    const localNames = Object.values(importAllLocalNames('default', state))
+
     return (
-      (t.isMemberExpression(tag) && tag.object.name === importLocalName('default', state)) ||
-      (t.isCallExpression(tag) && tag.callee.name === importLocalName('default', state)) ||
+      (t.isMemberExpression(tag) && localNames.includes(tag.object.name)) ||
+      (t.isCallExpression(tag) && localNames.includes(tag.callee.name)) ||
 
       /**
        * #93 Support require()
@@ -57,17 +62,17 @@ export const isStyled = (tag, state) => {
 
 export const isCSSHelper = (tag, state) => (
   t.isIdentifier(tag) &&
-  tag.name === importLocalName('css', state)
+  tag.name === importLocalName('css', state, 'styled-components')
 )
 
 export const isInjectGlobalHelper = (tag, state) => (
   t.isIdentifier(tag) &&
-  tag.name === importLocalName('injectGlobal', state)
+  tag.name === importLocalName('injectGlobal', state, 'styled-components')
 )
 
 export const isKeyframesHelper = (tag, state) => (
   t.isIdentifier(tag) &&
-  tag.name === importLocalName('keyframes', state)
+  tag.name === importLocalName('keyframes', state, 'styled-components')
 )
 
 export const isHelper = (tag, state) => (
