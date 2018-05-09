59a0da1360592e6b75c4817123523cbd0215db74
Evan Scott <probablyup@gmail.com>
2018-05-09 01:34:40 -0400

    rename fixtures to index.js to highlight the prefixing bug

--- test/index.test.js
+++ test/index.test.js
@@ -1,18 +1,24 @@
-import path from 'path'
-import fs from 'fs'
-import { transformFileSync } from 'babel-core'
-import plugin from '../src'
+import path from 'path';
+import fs from 'fs';
+import { transformFileSync } from 'babel-core';
+import plugin from '../src';
 
 describe('fixtures', () => {
-  const fixturesDir = path.join(__dirname, 'fixtures')
-  fs.readdirSync(fixturesDir).sort().map((caseName) => {
-    if (caseName === '.DS_Store') return
-    it(`should ${caseName.replace(/^\d*-/, '').split('-').join(' ')}`, () => {
-      const fixtureDir = path.join(fixturesDir, caseName)
-      const fixturePath = path.join(fixtureDir, 'before.js');
-      const fixture = transformFileSync(fixturePath).code
+    const fixturesDir = path.join(__dirname, 'fixtures');
+    fs
+        .readdirSync(fixturesDir)
+        .sort()
+        .map(caseName => {
+            if (caseName === '.DS_Store') return;
+            it(`should ${caseName
+                .replace(/^\d*-/, '')
+                .split('-')
+                .join(' ')}`, () => {
+                const fixtureDir = path.join(fixturesDir, caseName);
+                const fixturePath = path.join(fixtureDir, 'index.js');
+                const fixture = transformFileSync(fixturePath).code;
 
-      expect(fixture).toMatchSnapshot()
-    })
-  })
-})
+                expect(fixture).toMatchSnapshot();
+            });
+        });
+});
