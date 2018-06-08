d001d3e101c049fd643e42a4f8e3bf79483e1a93
Samatar.Axmed <samatar.axmed@livenation.co.uk>
2018-06-08 16:28:18 +0100

    Delete preprocess files in css folder

--- src/css/preprocess.js
+++
@@ -1,8 +0,0 @@
-import { temporaryClassname } from './placeholderUtils'
-
-import { convertOutputToBabelTypes, preprocessHelper } from './preprocessUtils'
-
-export default (cssArr, interpolationNodes) =>
-  convertOutputToBabelTypes(
-    preprocessHelper(cssArr, interpolationNodes, x => x, temporaryClassname)
-  )
