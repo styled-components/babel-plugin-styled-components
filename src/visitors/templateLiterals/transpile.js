15ed1cd7f863d3e110e5724436b8014d54fce988
Drew Pfundstein <drew.pfundstein@salesloft.com>
2018-08-14 18:46:26 -0400

    update templates with root node

--- src/visitors/templateLiterals/transpile.js
+++ src/visitors/templateLiterals/transpile.js
@@ -2,23 +2,30 @@ import * as t from 'babel-types'
 import { isStyled, isHelper } from '../../utils/detectors'
 import { useRootNode } from '../../utils/options'
 
-function addRoot(quasis, state) {
-  if (useRootNode(state)){
-    quasis[0].value.cooked = `${useRootNode(state)}& { ${quasis[0].value.cooked}`
-    quasis[quasis.length - 1].value.cooked = `${quasis[quasis.length - 1].value.cooked} }`
-    return quasis
-  } else {
-    return quasis
-  }
+function addRoot(quasi, state) {
+  quasi.quasis[0].value.cooked = `${useRootNode(state)}& { ${
+    quasi.quasis[0].value.cooked
+  }`
+  quasi.quasis[0].value.raw = `${useRootNode(state)}& { ${
+    quasi.quasis[0].value.raw
+  }`
+  quasi.quasis[quasi.quasis.length - 1].value.cooked = `${
+    quasi.quasis[quasi.quasis.length - 1].value.cooked
+  } }`
+  quasi.quasis[quasi.quasis.length - 1].value.raw = `${
+    quasi.quasis[quasi.quasis.length - 1].value.raw
+  } }`
+  return quasi
 }
 
 export default (path, state) => {
-  if (
-    isStyled(path.node.tag, state) ||
-    isHelper(path.node.tag, state)
-  ) {
-    const { tag: callee, quasi: { quasis, expressions } } = path.node
-
-    path.replaceWith( t.taggedTemplateExpression(callee, addRoot(quasis, state)))
+  if (isStyled(path.node.tag, state) || isHelper(path.node.tag, state)) {
+    const { tag, quasi } = path.node
+    if (
+      useRootNode(state) &&
+      !quasi.quasis[0].value.cooked.includes(useRootNode(state))
+    ) {
+      path.replaceWith(t.taggedTemplateExpression(tag, addRoot(quasi, state)))
+    }
   }
 }
