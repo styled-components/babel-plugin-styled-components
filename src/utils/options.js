11ce2b008d95457f89f0df8d572c3cab5b64fedf
Evan Scott <probablyup@gmail.com>
2018-08-21 00:48:00 -0500

    enable SSR-mode by default

--- src/utils/options.js
+++ src/utils/options.js
@@ -5,7 +5,7 @@ function getOption({ opts }, name, defaultValue = true) {
 }
 
 export const useDisplayName = state => getOption(state, 'displayName')
-export const useSSR = state => getOption(state, 'ssr', false)
+export const useSSR = state => getOption(state, 'ssr', true)
 export const useFileName = state => getOption(state, 'fileName')
 export const useMinify = state => getOption(state, 'minify')
 export const useTranspileTemplateLiterals = state =>
