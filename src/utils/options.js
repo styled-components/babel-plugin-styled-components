2bd778db2d8459aab1640aa27accec06446bbcf7
Evan Scott <probablyup@gmail.com>
2018-08-30 12:47:09 -0500

    only rewrite the styled-components import if >= v4

--- src/utils/options.js
+++ src/utils/options.js
@@ -1,3 +1,6 @@
+import semver from 'semver'
+import styledPkg from 'styled-components/package.json'
+
 function getOption({ opts }, name, defaultValue = true) {
   return opts[name] === undefined || opts[name] === null
     ? defaultValue
@@ -10,3 +13,10 @@ export const useFileName = state => getOption(state, 'fileName')
 export const useMinify = state => getOption(state, 'minify')
 export const useTranspileTemplateLiterals = state =>
   getOption(state, 'transpileTemplateLiterals')
+
+/**
+ * When using the babel plugin, we desugar styled.div to styled('div'), which means we can
+ * then use a lighter-weight version of s-c (v4+) since those element names don't need to be kept around
+ * ahead of time.
+ */
+export const useNoTags = () => semver.satisfies(styledPkg.version, '>= 4')
--- src/visitors/rewriteStyledImport.js
+++ src/visitors/rewriteStyledImport.js
@@ -1,10 +1,12 @@
+import { useNoTags } from '../utils/options'
+
 /**
- * When using the Babel plugin, we desugar styled.div to styled('div'), which means we can
- * then use a lighter-weight version of s-c since those element names don't need to be kept around
+ * When using the babel plugin, we desugar styled.div to styled('div'), which means we can
+ * then use a lighter-weight version of s-c (v4+) since those element names don't need to be kept around
  * ahead of time.
  */
 export default t => path => {
-  if (path.node.source.value === 'styled-components') {
+  if (useNoTags() && path.node.source.value === 'styled-components') {
     path.node.source = t.stringLiteral('styled-components/no-tags')
   }
 }
