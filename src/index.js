bc5fafd79857fbd42bfbed3e0d94ba1d77adf240
Evan Scott <probablyup@gmail.com>
2018-09-07 17:23:50 -0500

    remove desugaring and import rewriting functionality
    It was a good idea but ultimately doesn't work in certain cases. Also,
    since react-dom ships these strings anyway, the gains from no-tags backflips end
    up getting gzipped away.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import minify from './visitors/minify'
-import desugarStyled from './visitors/desugarStyled'
 import displayNameAndId from './visitors/displayNameAndId'
 import templateLiterals from './visitors/templateLiterals'
 import assignStyledRequired from './visitors/assignStyledRequired'
-import rewriteStyledImport from './visitors/rewriteStyledImport'
 
 export default function({ types: t }) {
   return {
     visitor: {
-      ImportDeclaration(path, state) {
-        rewriteStyledImport(t)(path, state)
-      },
-      MemberExpression(path, state) {
-        desugarStyled(t)(path, state)
-      },
       TaggedTemplateExpression(path, state) {
         minify(t)(path, state)
         displayNameAndId(t)(path, state)
