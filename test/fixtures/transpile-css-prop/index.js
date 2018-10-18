936eebaa190f9f2872515b13c45bac0f9dd1e9dd
Maximilian Stoiber <contact@mxstbr.com>
2018-10-18 09:36:08 +0200

    Integrate (failing) tests from babel-plugin-css-prop

---
+++ test/fixtures/transpile-css-prop/index.js
@@ -0,0 +1,56 @@
+/*
+ * Basic fixtures
+ */
+
+const StaticString = p => <p css="flex: 1;">A</p>
+
+const StaticTemplate = p => (
+  <p
+    css={`
+      flex: 1;
+    `}
+  >
+    A
+  </p>
+)
+
+const NoChildren = p => <p css="flex: 1;" />
+
+/*
+ * Dynamic prop
+ */
+
+const CustomComp = p => <Paragraph css="flex: 1">H</Paragraph>
+
+const DynamicProp = p => <p css={props.cssText}>H</p>
+
+const LocalInterpolation = p => (
+  <p
+    css={`
+      background: ${props.bg};
+    `}
+  >
+    H
+  </p>
+)
+
+const FuncInterpolation = p => (
+  <p
+    css={`
+      color: ${p => p.theme.a};
+    `}
+  >
+    H
+  </p>
+)
+
+const radius = 10
+const GlobalInterpolation = p => (
+  <p
+    css={`
+      border-radius: ${radius}px;
+    `}
+  >
+    H
+  </p>
+)
