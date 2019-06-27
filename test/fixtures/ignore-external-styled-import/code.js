9e652a710139440475d0cefe003c1bdf934b2601
Evan Jacobs <probablyup@gmail.com>
2019-06-27 12:51:13 -0500

    a little extra proof that the variable from s-c is tracked properly

--- test/fixtures/ignore-external-styled-import/code.js
+++ test/fixtures/ignore-external-styled-import/code.js
@@ -1,4 +1,9 @@
 import { styled } from '@material/ui'
+import s from 'styled-components'
 
-const Foo = p => <div {...p} />
+const Paragraph = s.p`
+  color: green;
+`
+
+const Foo = p => <Paragraph {...p} />
 const TestNormal = styled(Foo)({ color: red })
