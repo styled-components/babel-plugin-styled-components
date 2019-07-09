e593785a963a45af62915dba121f59bc550a2c51
Kai Hao <kevin830726@gmail.com>
2019-07-09 16:19:46 +0800

    Enable transpileTemplateLiterals in tests

--- test/fixtures/shim-unused-css-tag/output.js
+++ test/fixtures/shim-unused-css-tag/output.js
@@ -1,12 +1,12 @@
-import { css } from 'styled-components';
-const FLEX = 'flex';
-const flex = `
-  display: flex;
-`;
-const flexCSS = `display:flex;`;
-const flexCSSWithInterpolation = css(["display:", ";"], props => 'flex');
-const flexCSSWithConstantInterpolation = css(["display:", ";"], FLEX);
-const flexCSSFunction = css(`display: flex`);
-const Button = styled.button`
-  ${flexCSS}
-`;
+"use strict";
+
+var _styledComponents = require("styled-components");
+
+var FLEX = 'flex';
+var flex = "\n  display: flex;\n";
+var flexCSS = "display:flex;";
+var flexCSSWithInterpolation = (0, _styledComponents.css)(["display:", ";"], function (props) {
+  return 'flex';
+});
+var flexCSSWithConstantInterpolation = (0, _styledComponents.css)(["display:", ";"], FLEX);
+var flexCSSFunction = (0, _styledComponents.css)("display: flex");
