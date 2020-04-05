fd4d1b72db0ff927e08ad2bbbf24f13334443df9
rozzzly <pclienau@gmail.com>
2020-04-05 17:07:30 -0500

    tests for preferOuterAssignment (including some to ensure current behavior is not broken)

--- test/fixtures/prefer-outer-assignment-for-display-name/code.js
+++ test/fixtures/prefer-outer-assignment-for-display-name/code.js
@@ -10,12 +10,34 @@ const doubleStyles = { One: Discarded = styled.div`` }
 let Component
 Component = styled.div``
 const WrappedComponent = styled(Inner)``
+
 class ClassComponent {
   static Child = styled.div``
 }
+
 class DoubleClassComponent {
   static DoubleChild = Discarded = styled.div``
 }
+
 const DoubleAssignment = Discarded = styled.div`
+  width: 50%;
+`
+const TripleAssignment = Discarded2 = Discarded = styled.div`
+  width: 50%;
+`
+let DiscardWithLet;
+const UsingLetForDiscard = DiscardWithLet = styled.div`
+  width: 50%;
+`
+var DiscardWithVar;
+const UsingVarForDiscard = DiscardWithVar = styled.div`
+  width: 50%;
+`
+let UsingLetForAssignmentAfterDeclaration
+UsingLetForAssignmentAfterDeclaration = Discarded = styled.div`
+  width: 50%;
+`
+var UsingVarForAssignmentAfterDeclaration
+UsingVarForAssignmentAfterDeclaration = Discarded = styled.div`
   width: 50%;
 `
--- test/fixtures/prefer-outer-assignment-for-display-name/output.js
+++ test/fixtures/prefer-outer-assignment-for-display-name/output.js
@@ -17,7 +17,7 @@ const styles = {
 };
 const doubleStyles = {
   One: Discarded = styled.div.withConfig({
-    displayName: "Discarded"
+    displayName: "One"
   })``
 };
 let Component;
@@ -37,8 +37,27 @@ ClassComponent.Child = styled.div.withConfig({
 class DoubleClassComponent {}
 
 DoubleClassComponent.DoubleChild = Discarded = styled.div.withConfig({
-  displayName: "Discarded"
+  displayName: "DoubleChild"
 })``;
 const DoubleAssignment = Discarded = styled.div.withConfig({
-  displayName: "Discarded"
+  displayName: "DoubleAssignment"
+})`width:50%;`;
+const TripleAssignment = Discarded2 = Discarded = styled.div.withConfig({
+  displayName: "TripleAssignment"
+})`width:50%;`;
+let DiscardWithLet;
+const UsingLetForDiscard = DiscardWithLet = styled.div.withConfig({
+  displayName: "UsingLetForDiscard"
+})`width:50%;`;
+var DiscardWithVar;
+const UsingVarForDiscard = DiscardWithVar = styled.div.withConfig({
+  displayName: "UsingVarForDiscard"
+})`width:50%;`;
+let UsingLetForAssignmentAfterDeclaration;
+UsingLetForAssignmentAfterDeclaration = Discarded = styled.div.withConfig({
+  displayName: "UsingLetForAssignmentAfterDeclaration"
+})`width:50%;`;
+var UsingVarForAssignmentAfterDeclaration;
+UsingVarForAssignmentAfterDeclaration = Discarded = styled.div.withConfig({
+  displayName: "UsingVarForAssignmentAfterDeclaration"
 })`width:50%;`;
