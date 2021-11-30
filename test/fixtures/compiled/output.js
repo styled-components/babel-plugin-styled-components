ef3667f50df11f37ffba209391b1a127bcb62c05
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2021-11-30 12:56:19 +0100

    commonjs

---
+++ test/fixtures/compiled/output.js
@@ -0,0 +1,8 @@
+var _styled = _interopRequireDefault(require("styled-components"));
+
+const Test = (0, _styled.default)('div').withConfig({
+  displayName: 'Test',
+  componentId: 'sc-2jen0y-0',
+})({
+  width: '100%'
+});
